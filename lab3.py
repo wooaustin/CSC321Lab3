c340414f09dc8b6bb51db3bf4513d7acb7de3326
Austin Woo <wooaustin0@gmail.com>
2019-10-10 13:36:08 -0700

    Started Task1b, need to truncate to 8 bits

--- lab3.py
+++ lab3.py
@@ -4,11 +4,35 @@ def process_sha(input):
     h = SHA256.new()
     h.update(input)
     print(h.hexdigest())
+    return h.hexdigest()
+
+
+def compare_strings(a, b):
+    count = 0
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            count += 1
+    print(count)
+
+    
+def truncate_digest(d1, d2):
+    mask_8 = 255
+    masked_d1 = ord(d1) & mask_8
+    masked_d2 = ord(d2) & mask_8
+    
+    
+def task1_generate_digest():
+    a = process_sha(b'tesr')
+    b = process_sha(b'tess')
+    compare_strings(a, b)
+    return a,b
 
 
 def main():
-    # Task 1
-    process_sha(b'tesr')
-    process_sha(b'tess')
+    # Task 1a
+    d1, d2 = task1_generate_digest()
+    
+    # Task 1b
+    truncate_digest(d1, d2)
 
 main()
