c52e6efc15982486bf0b2ecda5d172a3817b8e6d
ShaheenAlemi <46461452+cpshaheen@users.noreply.github.com>
2019-10-10 18:40:21 -0700

    almost there: 8bit collision found, now we gotta loop over bitsizes and perm sizes and save time + other metrics for graphing

--- lab3.py
+++ lab3.py
@@ -1,4 +1,8 @@
 from Crypto.Hash import SHA256
+import math
+from itertools import product
+import os
+import binascii
 
 def process_sha(input):
     h = SHA256.new()
@@ -14,7 +18,7 @@ def compare_strings(a, b):
             count += 1
     print(count)
 
-    
+
 def return_bits(d_bytes):
     a = []
     for i in range(len(d_bytes)):
@@ -22,36 +26,53 @@ def return_bits(d_bytes):
             break
         a.append(d_bytes[i])
     return a
-    
-# For a given string input, compute the digest and return the truncated 8-bits    
-def truncate_digest(d1, x):
+
+# For a given string input, compute the digest and return the truncated 8-bits
+def truncate_digest(str, num_bits):
     scale = 16
-    num_bits = 8
+    hex_val = math.ceil(num_bits/4)
     h = SHA256.new()
-    h.update(d1)
+    h.update(str.encode('utf-8'))
     digest = h.hexdigest()
-    digest_array = digest[:3]
-    print(digest_array)
-    bit_str = bin(int(digest_array, scale))[x:].zfill(num_bits)
-    num_bits = 10
+    digest_array = digest[:hex_val]
+    #print(digest_array)
+    bit_str = bin(int(digest_array, scale))[2:].zfill(num_bits)
     bit_str_msb = bit_str[:num_bits]
-    print(bit_str_msb, bit_str_lsb)
-    #truncate_d1 = return_bits(d1_bytes)
-    #truncate_d2 = return_bits(d2_bytes)
-    
-    
+    #print(bit_str_msb)
+    return bit_str_msb
+    #print(bit_str_msb, bit_str_lsb)
+
+# given a length this function will
+# create all possible strs of that length
+def str_perm_gen(str_len):
+    return([''.join(x) for x in product('ABCDEFGHIJKLMNOPQRSTUVWXYZ', repeat=str_len)])
+
 def task1_generate_digest():
     a = process_sha(b'tesr')
     b = process_sha(b'tess')
     compare_strings(a, b)
     return a,b
 
+def task2():
+    perm_list = str_perm_gen(6)
+    bit_size = 44
+    collision_dict = {}
+    for perm in perm_list:
+        trunced_dig = truncate_digest(perm,bit_size)
+        if trunced_dig in collision_dict:
+            #print(collision_dict)
+            print(trunced_dig)
+            print(collision_dict[trunced_dig], perm)
+            exit()
+        else:
+            collision_dict[trunced_dig] = perm
 
 def main():
     # Task 1a
     #d1, d2 = task1_generate_digest()
-    
+
     # Task 1b
-    truncate_digest(b"test", 2)
+    task2()
+    #truncate_digest(b"test", 2)
 
 main()
