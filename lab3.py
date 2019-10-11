337d72ba871101b47815cf0905994beb6b5a5ab3
Austin Woo <wooaustin0@gmail.com>
2019-10-10 17:41:39 -0700

    truncated bits

--- lab3.py
+++ lab3.py
@@ -3,7 +3,7 @@ from Crypto.Hash import SHA256
 def process_sha(input):
     h = SHA256.new()
     h.update(input)
-    print(h.hexdigest())
+    print(type(h.hexdigest()))
     return h.hexdigest()
 
 
@@ -23,13 +23,21 @@ def return_bits(d_bytes):
         a.append(d_bytes[i])
     return a
     
-    
-def truncate_digest(d1, d2):
-    b = bytearray()
-    d1_bytes = b.extend(map(ord,d1))
-    d2_bytes = b.extend(map(ord,d2))
-    truncate_d1 = return_bits(d1_bytes)
-    truncate_d2 = return_bits(d2_bytes)
+# For a given string input, compute the digest and return the truncated 8-bits    
+def truncate_digest(d1, x):
+    scale = 16
+    num_bits = 8
+    h = SHA256.new()
+    h.update(d1)
+    digest = h.hexdigest()
+    digest_array = digest[:3]
+    print(digest_array)
+    bit_str = bin(int(digest_array, scale))[x:].zfill(num_bits)
+    num_bits = 10
+    bit_str_msb = bit_str[:num_bits]
+    print(bit_str_msb, bit_str_lsb)
+    #truncate_d1 = return_bits(d1_bytes)
+    #truncate_d2 = return_bits(d2_bytes)
     
     
 def task1_generate_digest():
@@ -41,9 +49,9 @@ def task1_generate_digest():
 
 def main():
     # Task 1a
-    d1, d2 = task1_generate_digest()
+    #d1, d2 = task1_generate_digest()
     
     # Task 1b
-    truncate_digest(d1, d2)
+    truncate_digest(b"test", 2)
 
 main()
